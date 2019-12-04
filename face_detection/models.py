f50e268733ccec1290a64db553c2c8346598ab3f
laneboi <andrei.kulchik@gmail.com>
2019-12-04 18:34:41 +0300

    Fix auto update of `first_visit` on instance change

--- face_detection/models.py
+++ face_detection/models.py
@@ -16,7 +16,8 @@ class BaseCustomer(models.Model):
 
     name = models.CharField(max_length=100, null=False, default='<Unknown>')
     sex = models.CharField(max_length=1, null=False, choices=SEX_CHOICES)
-    first_visit = models.DateTimeField(verbose_name='Date of first visit', auto_now=True)
+    first_visit = models.DateTimeField(verbose_name='Date of first visit', auto_now_add=True)
+    last_visit = models.DateTimeField(verbose_name='Date of last visit', auto_now=True)
     spent = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
 
 
